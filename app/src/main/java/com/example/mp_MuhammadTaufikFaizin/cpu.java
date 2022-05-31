e68a86ccdf4c52151c891763fc533a0717e55c12
MuhammadTaufikFaizin <faizintaufik420@gmail.com>
2022-05-31 16:32:17 +0700

    UAS

---
+++ app/src/main/java/com/example/mp_MuhammadTaufikFaizin/cpu.java
@@ -0,0 +1,15 @@
+package com.example.mp_muhammadalipmutakin;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cpu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cpu);
+    }
+}
